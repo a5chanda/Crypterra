63b33d20753db686c20f1d990e47aa16f27bb190
Ryan Min <ryanmin42@gmail.com>
2018-04-25 20:38:14 -0400

    debugging

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 var http = require('http');
-var request = require('request');
 var express = require('express');
-var BusinessNetworkConnection = require('composer-common').BusinessNetworkConnection;
+const BusinessNetworkConnection = require('composer-common').BusinessNetworkConnection;
 var bodyParser = require('body-parser');
 var app = express();
-
-const connection = new BusinessNetworkConnection();
-await connection.connect(cardName);
+this.bizNetworkConnection = new BusinessNetworkConnection();
+this.businessNetworkDefinition = this.bizNetworkConnection.connect('admin@deloitte-chain');
 let factory = getFactory();
-let transactionRegistry = await connection.getAssetRegistry('net.biz.digitalPropertyNetwork.Transactions');
-let companyRegistry = await connection.getParticipantRegistry('net.biz.digitalPropertyNetwork.Company');
-let personRegistry = await connection.getParticipantRegistry('net.biz.digitalPropertyNetwork.Person');
+let transactionRegistry = this.bizNetworkConnection.getAssetRegistry('net.biz.digitalPropertyNetwork.Transactions');
+let companyRegistry = this.bizNetworkConnection.getParticipantRegistry('net.biz.digitalPropertyNetwork.Company');
+let personRegistry = this.bizNetworkConnection.getParticipantRegistry('net.biz.digitalPropertyNetwork.Person');
 
 app.use('/', express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({
-    extended: false;
+    extended: false
 }));
 app.use(bodyParser.json());
 app.listen(3000);
@@ -29,6 +27,7 @@ app.post('/excel', function(req, res) {
   res.json(ledger);
 });
 
+
 app.post('/admin', function(req, res) {
   var data = req.body.data;
   var retVal = addCompany(data);
@@ -39,19 +38,20 @@ function addCompany(data) {
   var company = factory.newResource('', 'Company', data.id);
   company.name = data.name;
   company.ceo = addPerson(data.ceo);
-  int length = data.employeeCount;
+  var length = data.employeeCount;
   company.employeeCount = length;
   for(var i = 0; i < length; i++) {
     company.employees[i] = addPerson(data.employees[i]);
   }
   company.description = data.description;
-  int length2 = data.subsidiaryCount;
+  var length2 = data.subsidiaryCount;
   company.subsidiaryCount = length2;
   for(var i = 0; i < length2; i++) {
     company.subsidiaries[i] = addCompany(data.subsidiaries[i]);
   }
   company.location = data.location;
-  await companyRegistry.add(company);
+  company.cashBalance = data.cashBalance;
+  companyRegistry.add(company);
   return company;
 }
 
@@ -60,7 +60,7 @@ function addPerson(data) {
   person.name = data.name;
   person.title = data.title;
   person.salary = data.salary;
-  await personRegistry.add(person);
+  personRegistry.add(person);
   return person;
 }
 
@@ -73,6 +73,6 @@ function addTransaction(data) {
   transaction.receiver = data.receiver;
   transaction.receivingCurrency = data.receivingCurrency;
   transaction.description = data.description;
-  await transactionRegistry.add(transaction);
+  transactionRegistry.add(transaction);
   return transaction;
 }
