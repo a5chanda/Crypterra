e0b39aa0edb71e124b35ada771b34ede85aa5652
Abhinav Chanda <abhinav.chanda99@gmail.com>
2018-04-27 02:53:58 -0400

    Updated transactions
    All transactions set up

--- deloitte-chain/lib/logic.js
+++ deloitte-chain/lib/logic.js
@@ -18,26 +18,128 @@
  */
 
 /**
- * Sample transaction
+ * Bitcoin transaction
  * @param {org.deloitte.net.btcTransaction} btcTransaction
  * @transaction
  */
-function btcTransaction(btcTransfer) {
-    if(btcTransfer.from.balance < btcTransfer.amountBTC){
+function btcTransaction(btcTransferAccount){
+    if(btcTransferAccount.from.balance < btcTransferAccount.amountBTC){
         throw new Error ("Insufficient funds");
     }
     else{
-        btcTransfer.from.balance -= btcTransfer.amountBTC;
-        btcTransfer.to.balance += btcTransfer.amountBTC;
+        btcTransferAccount.from.balanceBTC -= btcTransferAccount.amountBTC;
+        btcTransferAccount.to.balanceBTC += btcTransferAccount.amountBTC;
 
         return getAssetRegistry('org.deloitte.net.btcAccount').then (function (assetRegistry) {
-            return assetRegistry.update(btcTransfer.from);
+            return assetRegistry.update(btcTransferAccount.from);
         })
-        .then (function () {
+        .then (function (){
         return getAssetRegistry('org.deloitte.net.btcAccount');
         })
         .then(function (assetRegistry) {
-        return assetRegistry.update(btcTransfer.to);
+        return assetRegistry.update(btcTransferAccount.to);
+        });
+    }
+}
+
+/**
+ * ETHEREUM transaction
+ * @param {org.deloitte.net.ethTransaction} ethTransaction
+ * @transaction
+ */
+function ethTransaction(ethTransferAccount) {
+    if(ethTransferAccount.from.balance < ethTransferAccount.amountETH){
+        throw new Error ("Insufficient funds");
+    }
+    else{
+        ethTransferAccount.from.balanceETH -= ethTransferAccount.amountETH;
+        ethTransferAccount.to.balanceETH += ethTransferAccount.amountETH;
+
+        return getAssetRegistry('org.deloitte.net.ethAccount').then (function (assetRegistry) {
+            return assetRegistry.update(ethTransferAccount.from);
+        })
+        .then (function () {
+        return getAssetRegistry('org.deloitte.net.ethAccount');
+        })
+        .then(function (assetRegistry) {
+        return assetRegistry.update(ethTransferAccount.to);
+        });
+    }
+}
+
+
+/**
+ * USD transaction
+ * @param {org.deloitte.net.usdTransaction} usdTransaction
+ * @transaction
+ */
+function usdTransaction(usdTransferAccount) {
+    if(usdTransferAccount.from.balance < usdTransferAccount.amountUSD){
+        throw new Error ("Insufficient funds");
+    }
+    else{
+        usdTransferAccount.from.balanceUSD -= usdTransferAccount.amountUSD;
+        usdTransferAccount.to.balanceUSD += usdTransferAccount.amountUSD;
+
+        return getAssetRegistry('org.deloitte.net.usdAccount').then (function (assetRegistry) {
+            return assetRegistry.update(usdTransferAccount.from);
+        })
+        .then (function () {
+        return getAssetRegistry('org.deloitte.net.usdAccount');
+        })
+        .then(function (assetRegistry) {
+        return assetRegistry.update(usdTransferAccount.to);
+        });
+    }
+}
+
+
+/**
+ * CAD transaction
+ * @param {org.deloitte.net.cadTransaction} cadTransaction
+ * @transaction
+ */
+function cadTransaction(cadTransferAccount) {
+    if(cadTransferAccount.from.balance < cadTransferAccount.amountCAD){
+        throw new Error ("Insufficient funds");
+    }
+    else{
+        cadTransferAccount.from.balanceCAD -= cadTransferAccount.amountCAD;
+        cadTransferAccount.to.balanceCAD += cadTransferAccount.amountCAD;
+
+        return getAssetRegistry('org.deloitte.net.cadAccount').then (function (assetRegistry) {
+            return assetRegistry.update(cadTransferAccount.from);
+        })
+        .then (function () {
+        return getAssetRegistry('org.deloitte.net.cadAccount');
+        })
+        .then(function (assetRegistry) {
+        return assetRegistry.update(cadTransferAccount.to);
+        });
+    }
+}
+
+/**
+ * GBP transaction
+ * @param {org.deloitte.net.gbpTransaction} gbpTransaction
+ * @transaction
+ */
+function gbpTransaction(gbpTransferAccount) {
+    if(gbpTransferAccount.from.balance < gbpTransferAccount.amountGBP){
+        throw new Error ("Insufficient funds");
+    }
+    else{
+        gbpTransferAccount.from.balanceGBP -= gbpTransferAccount.amountGBP;
+        gbpTransferAccount.to.balanceGBP += gbpTransferAccount.amountGBP;
+
+        return getAssetRegistry('org.deloitte.net.gbpAccount').then (function (assetRegistry) {
+            return assetRegistry.update(gbpTransferAccount.from);
+        })
+        .then (function () {
+        return getAssetRegistry('org.deloitte.net.gbpAccount');
+        })
+        .then(function (assetRegistry) {
+        return assetRegistry.update(gbpTransferAccount.to);
         });
     }
 }
