a05728534c99a27702e2e66431fbb6efc249deab
Ryan Min <ryanmin42@gmail.com>
2018-04-28 00:35:35 -0400

    changed up rest api

--- deloitte-chain/server.js
+++ deloitte-chain/server.js
@@ -5,9 +5,16 @@ const BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefi
 var bodyParser = require('body-parser');
 var app = express();
 let factory;
+var companyId;
 let companyRegistry;
 let personRegistry;
 let businessNetworkDefinition;
+let bitcoinRegistry;
+let ethereumRegistry;
+let canadaRegistry;
+let americaRegistry;
+let britainRegistry;
+
 app.use('/', express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({
     extended: false
@@ -16,70 +23,108 @@ app.use(bodyParser.json());
 app.listen(3000);
 let bizNetConnection = new BusinessNetworkConnection();
 (async () => {
-  businessNetworkDefinition = await bizNetConnection.connect('admin@deloitte-chain');
+  businessNetworkDefinition = await bizNetConnection.connect('../admin@deloitte-chain');
   factory = businessNetworkDefinition.getFactory();
-  let transactionRegistry = await connection.getAssetRegistry('org.deloitte.net.Transactions');
+  bitcoinRegistry = await connection.getTransactionRegistry('org.deloitte.net.btcTransaction');
+  ethereumRegistry = await connection.getTransactionRegistry('org.deloitte.net.ethTransaction');
+  canadaRegistry = await connection.getTransactionRegistry('org.deloitte.net.cadTransaction');
+  americaRegistry = await connection.getTransactionRegistry('org.deloitte.net.usdTransaction');
+  britainRegistry = await connection.getTransactionRegistry('org.deloitte.net.gbpTransaction');
   companyRegistry = await connection.getParticipantRegistry('org.deloitte.net.Company');
   personRegistry = await connection.getParticipantRegistry('org.deloitte.net.Employee');
-})();
-//let businessNetworkDefinition;
-
-app.post('/admin', function(req, res) {
-  var data = req.body.data;
-  var retVal = addCompany(data);
-  res.json(retVal);
+})().catch(function(err) {
+  console.log(err);
 });
 
-app.post('/excel', function(req, res) {
-  var sheet = req.body.data.sheets.sheet1;
+app.post('/admin', function(req, res) {
+  var companyData = req.body.company;
+  companyId = addCompany(companyData);
+  var sheet = req.body.sheet;
   var length = sheet.length;
-  var ledger = [];
   for(var i = 0; i < length; i++) {
-    ledger[i] = addTransaction(sheet[i]);
+    addTransaction(sheet[i]);
   }
-  res.json(ledger);
 });
 
 function addCompany(data) {
   var company = factory.newResource('org.deloitte.net', 'Company', data.id);
   company.companyName = data.companyName;
-  company.companyID = data.id;
-  company.ceo = addemployee(data.ceo);
-  var length = data.employeeCount;
-  company.employeeCount = length;
+  company.companyId = data.id;
+  company.netWorth = parseFloat(data.netWorth);
+  company.ceo = data.ceo;
+  company.description = data.description;
+  company.location = data.location;
+  company.btcBalance = parseFloat(data.btcBalance);
+  company.ethBalance = parseFloat(data.ethBalance);
+  company.cadBalance = parseFloat(data.cadBalance);
+  company.usdBalance = parseFloat(data.usdBalance);
+  company.gbpBalance = parseFloat(data.gbpBalance);
+  var length = company.employees.length;
   for(var i = 0; i < length; i++) {
     company.employees[i] = addEmployee(data.employees[i]);
   }
-  company.description = data.description;
-  var length2 = data.subsidiaryCount;
-  company.subsidiaryCount = length2;
+  var length2 = data.subsidiaries.length;
   for(var i = 0; i < length2; i++) {
     company.subsidiaries[i] = addCompany(data.subsidiaries[i]);
   }
-  company.location = data.location;
-  company.cashBalance = data.cashBalance;
   companyRegistry.add(company);
   return data.id;
 }
 
 function addEmployee(data) {
-  var employee = factory.newResource('org.deloitte.net', 'Employee', data.id);
-  employee.employeeID = data.id;
+  var employee = factory.newResource('org.deloitte.net', 'Employee', data.Id);
+  employee.employeeID = data.Id;
   employee.firstName = data.firstName;
   employee.lastName = data.lastName;
+  employee.position = data.Position;
+  employee.salary = parseFloat(data.Salary);
+  employee.btcBalance = parseFloat(data.btcBalance);
+  employee.ethBalance = parseFloat(data.ethBalance);
+  employee.cadBalance = parseFloat(data.cadBalance);
+  employee.usdBalance = parseFloat(data.usdBalance);
+  employee.gbpBalance = parseFloat(data.gbpBalance);
   employeeRegistry.add(employee);
-  return data.id;
+  return data.Id;
 }
 
 function addTransaction(data) {
-  var transaction = factory.newResource('org.deloitte.net', 'Transaction', data.id);
-  transaction.date = data.date;
-  transaction.sender = data.sender;
-  transaction.amount = data.amount;
-  transaction.sentCurrency = data.sentCurrency;
-  transaction.receiver = data.receiver;
-  transaction.receivingCurrency = data.receivingCurrency;
-  transaction.description = data.description;
-  transactionRegistry.add(transaction);
-  return transaction;
+  var transaction;
+  var currency;
+  if(data.Debit.length > 0)
+    currency = data.Debit.slice(-3);
+  else
+    currency = data.Credit.slice(-3);
+  if(currency.toUpperCase() === 'BTC')
+    transaction = factory.newResource('org.deloitte.net', 'btcTransaction', data.Id);
+  else if(currency.toUpperCase() === 'ETH')
+    transaction = factory.newResource('org.deloitte.net', 'ethTransaction', data.Id);
+  else if(currency.toUpperCase() === 'CAD')
+    transaction = factory.newResource('org.deloitte.net', 'cadTransaction', data.Id);
+  else if(currency.toUpperCase() === 'USD')
+    transaction = factory.newResource('org.deloitte.net', 'usdTransaction', data.Id);
+  else if(currency.toUpperCase() === 'GBP')
+    transaction = factory.newResource('org.deloitte.net', 'gbpTransaction', data.Id);
+  transaction.transactionID = data.Id;
+  transaction.date = data.Date;
+  if(data.Debit.length > 0) {
+    transaction.from = companyID;
+    transaction.to = data.ParticipantId;
+    transaction.amount = data.Debit.slice(1, -3);
+  }
+  else {
+    transaction.from = data.ParticipantId;
+    transaction.to = companyID;
+    transaction.amount = data.Credit.slice(1, -3);
+  }
+  transaction.description = data.Description;
+  if(currency.toUpperCase() === 'BTC')
+    bitcoinRegistry.add(transaction);
+  else if(currency.toUpperCase() === 'ETH')
+    ethereumRegistry.add(transaction);
+  else if(currency.toUpperCase() === 'CAD')
+    canadaRegistry.add(transaction);
+  else if(currency.toUpperCase() === 'USD')
+    americaRegistry.add(transaction);
+  else if(currency.toUpperCase() === 'GBP')
+    britainRegistry.add(transaction);
 }
